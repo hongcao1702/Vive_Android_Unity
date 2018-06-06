cd04029ee615e839d3ba03c9f92b1cb11d1a127b
hongcao1702 <hongcao1985@hotmail.com>
2018-06-06 13:59:28 +0800

    新增GVoice语音库

--- app/src/main/java/com/second/flytek/activity/FlyTekActivity.java
+++ app/src/main/java/com/second/flytek/activity/FlyTekActivity.java
@@ -1,15 +1,31 @@
 package com.second.flytek.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.TextUtils;
+import android.util.Log;
+
 import com.htc.vr.unity.WVRUnityVRActivity;
 import com.second.flytek.setting.Qisr;
 import com.second.flytek.setting.Qtts;
+import com.tencent.gcloud.voice.GCloudVoiceEngine;
+import com.tencent.gcloud.voice.IGCloudVoiceNotify;
 
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class FlyTekActivity extends WVRUnityVRActivity {
     private Qtts mQtts = null;
     private Qisr mQisr = null;
+
+    private static GCloudVoiceEngine mCloudEngine = null;
+    private static String appID = "1318065628";
+    private static String appKey = "8d5d31cf4b1d425c9d17133c7563cdab";
+    private static String openID = Long.toString(System.currentTimeMillis());
+    private static String roomName = "cz-test";
+
+    public final String tag = "GCloudVoiceNotify";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +37,27 @@ public class FlyTekActivity extends WVRUnityVRActivity {
         if(mQisr == null) {
             mQisr = new Qisr(this);
         }
+
+        // Important
+        GCloudVoiceEngine.getInstance().init(getApplicationContext(), this);
+        mCloudEngine = GCloudVoiceEngine.getInstance();
+        mCloudEngine.SetAppInfo(appID, appKey, openID);
+        mCloudEngine.Init();
+        mCloudEngine.SetMode(0);
+        Notify notify = new Notify();
+        mCloudEngine.SetNotify(notify);
+
+        //timer to poll
+        TimerTask task = new TimerTask() {
+            public void run() {
+                Message message = new Message();
+                message.what = 1;
+                handler.sendMessage(message);
+            }
+        };
+
+        Timer timer = new Timer(true);
+        timer.schedule(task, 500, 500);
     }
 
     public void setText(String data) {
@@ -38,6 +75,108 @@ public class FlyTekActivity extends WVRUnityVRActivity {
             mQisr.stopRecognize();
         }
         super.onDestroy();
+    }
+
+    final Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    mCloudEngine.Poll();
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    class Notify implements IGCloudVoiceNotify {
+        /*
+        String TAG = "[cz]";
+        @Override
+        public void onJoinRoomComplete(int code) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onJoinRoomComplete:" + code);
+            _logTV.setText( "onJoinRoomComplete with "+ code);
+        }*/
+
+
+        @Override
+        public void OnJoinRoom(int code, String roomName, int memberID) {
+            Log.i(tag, "OnJoinRoom CallBack code=" + code + " roomname:" + roomName + " memberID:" + memberID);
+            setText("OnJoinRoom CallBack code=" + code + " roomname:" + roomName + " memberID:" + memberID);
+        }
+
+        @Override
+        public void OnStatusUpdate(int status, String roomName, int memberID) {
+            Log.i(tag, "OnStatusUpdate CallBack code=" + status + " roomname:" + roomName + " memberID:" + memberID);
+            setText("OnStatusUpdate CallBack code=" + status + " roomname:" + roomName + " memberID:" + memberID);
+        }
+
+        @Override
+        public void OnQuitRoom(int code, String roomName) {
+            Log.i(tag, "OnQuitRoom CallBack code=" + code + " roomname:" + roomName);
+            setText("OnQuitRoom CallBack code=" + code + " roomname:" + roomName);
+        }
+
+        @Override
+        public void OnMemberVoice(int[] members, int count) {
+            Log.i(tag, "OnMemberVoice CallBack " + "count:" + count);
+            String str = "OnMemberVoice Callback ";
+            for (int i = 0; i < count; ++i) {
+                str += " memberid:" + members[2 * i];
+                str += " state:" + members[2 * i + 1];
+            }
+            setText("OnMemberVoice CallBack " + "count:" + count);
+        }
+
+        @Override
+        public void OnUploadFile(int code, String filePath, String fileID) {
+            Log.i(tag, "OnUploadFile CallBack code=" + code + " filePath:" + filePath + " fileID:" + fileID);
+            setText("OnUploadFile CallBack code=" + code + " filePath:" + filePath + " fileID:" + fileID);
+        }
+
+        @Override
+        public void OnDownloadFile(int code, String filePath, String fileID) {
+            Log.i(tag, "OnDownloadFile CallBack code=" + code + " filePath:" + filePath + " fileID:" + fileID);
+            setText("OnDownloadFile CallBack code=" + code + " filePath:" + filePath + " fileID:" + fileID);
+        }
+
+        @Override
+        public void OnPlayRecordedFile(int code, String filePath) {
+            Log.i(tag, "OnPlayRecordedFile CallBack code=" + code + " filePath:" + filePath);
+            setText("OnPlayRecordedFile CallBack code=" + code + " filePath:" + filePath);
+        }
+
+        @Override
+        public void OnApplyMessageKey(int code) {
+            Log.i(tag, "OnApplyMessageKey CallBack code=" + code);
+           setText("OnApplyMessageKey CallBack code=" + code);
+        }
+
+        @Override
+        public void OnSpeechToText(int code, String fileID, String result) {
+            Log.i(tag, "OnSpeechToText CallBack code=" + code + " fileID:" + fileID + " result:" + result);
+            setText("OnSpeechToText CallBack code=" + code + " fileID:" + fileID + " result:" + result);
+        }
+
+        @Override
+        public void OnRecording(char[] pAudioData, int nDataLength) {
+            Log.i(tag, "OnRecording CallBack  nDataLength:" + nDataLength);
+        }
+
+        @Override
+        public void OnStreamSpeechToText(int i, int i1, String s, String s1) {
+
+        }
+
+        @Override
+        public void OnMemberVoice(String s, int i, int i1) {
+
+        }
+
+        @Override
+        public void OnRoleChanged(int i, String s, int i1, int i2) {
+
+        }
     }
 
     /****************************************************************************************
@@ -116,6 +255,34 @@ public class FlyTekActivity extends WVRUnityVRActivity {
     public void MSG_SetTTSVoicer(String voicer) {
         if(mQtts != null) {
             mQtts.setVoicer(voicer);
+        }
+    }
+
+    public void MSG_JoinRoom(){
+        if(mCloudEngine != null) {
+            Log.d(tag,"+++MSG_JoinRoom");
+            mCloudEngine.JoinTeamRoom(roomName,10000);
+        }
+    }
+
+    public void MSG_OpenMic(){
+        if(mCloudEngine != null) {
+            Log.d(tag,"+++MSG_OpenMic");
+            mCloudEngine.OpenMic();
+        }
+    }
+
+    public void MSG_OpenSpeaker(){
+        if(mCloudEngine != null) {
+            Log.d(tag,"+++MSG_OpenSpeaker");
+            mCloudEngine.OpenSpeaker();
+        }
+    }
+
+    public void MSG_QuitRoom(){
+        if(mCloudEngine != null) {
+            Log.d(tag,"+++MSG_QuitRoom");
+            mCloudEngine.QuitRoom(roomName,10000);
         }
     }
 
